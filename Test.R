04a180fe9b66fcbdc7fd98d61850d376ab0ec000
dbthomas <d.thomas.inms@gmail.com>
2018-02-06 11:59:09 +1300

    02 Feb

--- Test.R
+++ Test.R
@@ -1,1 +1,9 @@
 test
+
+install.packages("repmis")
+library(repmis)
+
+https://www.dropbox.com/sh/s9nljxduim14z94/AACSY7yW5fJln__c5eyh14MYa?dl=0
+
+?source_DropboxData
+list.files("C:/Users/User/Dropbox/Canterbury Museum 3D files/AV37318 Acanthisitta chloris")
